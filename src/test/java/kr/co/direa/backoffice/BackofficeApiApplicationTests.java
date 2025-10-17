3aa368832e6e43684d091893ea64117d0e16c7d8
hyeonkyeong.park <hkpark@direa.co.kr>
2025-10-17 11:28:51 +0900

    first init

---
+++ src/test/java/kr/co/direa/backoffice/BackofficeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.direa.backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackofficeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
